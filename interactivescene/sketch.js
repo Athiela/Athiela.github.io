b39e0bc7d2b2f4d95d5d1de6f5f8438a18ec637b
Athiela A <athiela_arcamo@yahoo.ca>
2021-09-21 11:00:47 -0600

    interactive scene hover button work

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -9,13 +9,11 @@
 //////////// Global Variables ///////////////////////////
 
 let cardR, cardP, cardS, xCardR, yCardR, xCardP, yCardP, xCardS, yCardS, cardWidthRock, cardHR, cardWP, cardHP, cardWS, cardHS, title, rockFrame, paperFrame, scissorsFrame, playButton, howToPlayButton;
+let playButtonWidth, howToPlayButtonWidth, playButtonHeight, howToPlayButtonHeight, xHowToPlayButton, yHowToPlayButton, xPlayButton, yPlayButton;
+let hoverHowToPlay, hoverPlay;
 
-let state = "play";
+let state = "start";
 
-let playButtonWidth = 200;
-let howToPlayButtonWidth = 200;
-let playButtonHeight = 100;
-let howToPlayButtonHeight = 100;
 
 let rockHover = false;
 let paperHover = false;
@@ -39,6 +37,8 @@ function preload() {
   playButton = loadImage("assets/playButton.png");
   howToPlayButton = loadImage("assets/howToPlayButton.png");
   
+  hoverPlay = loadImage("assets/hoverPlayButton.png");
+  hoverHowToPlay = loadImage("assets/hoverHowToPlayButton.png");
 }
 
 /////////////////// Setup //////////////////////////
@@ -46,13 +46,13 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
-  xCardR = width/10;
+  xCardR = width/4;
   yCardR = height/2;
   
   xCardP = width/2;
   yCardP = height/2;
   
-  xCardS = width/1.12;
+  xCardS = width/1.3;
   yCardS = height/2;
   cardWidthRock = width/6;
   cardHR = height/2.2;
@@ -62,6 +62,18 @@ function setup() {
   
   cardWS = width/6;
   cardHS = height/2.2;
+
+  xHowToPlayButton = width/2;
+  yHowToPlayButton = height/1.3;
+
+  xPlayButton = width/2;
+  yPlayButton = height/2;
+
+  playButtonWidth = width/4;
+  howToPlayButtonWidth = width/4;
+
+  playButtonHeight = height/5;
+  howToPlayButtonHeight = height/5;
 }
 
 function draw() {
@@ -74,12 +86,12 @@ function draw() {
 
 function gameState() {
   if (state === "start") {
-    background(3);
     displayPlayButton();
     displayHowToPlayButton();
     displayStartTitle();
 
     hoverStartButtons();
+    pressPlay();
     
   }
   if (state === "play") {
@@ -98,7 +110,7 @@ function gameState() {
 ////////// Display image functions /////////////////
 
 function displayTitle() {
-  image(title, width / 2, height / 7, 200, 100);
+  image(title, width / 2, height /7, 200, 100);
 }
 
 function displayCardR() {
@@ -133,19 +145,37 @@ function displayScissorsFrame() {
 ///////////// Game = Start Functions /////////////////////
 
 function displayPlayButton() {
-  image(playButton, width/2, height/1.7, playButtonWidth, playButtonHeight);
+  image(playButton, xPlayButton, yPlayButton, playButtonWidth, playButtonHeight);
 }
 function displayHowToPlayButton() {
-  image(howToPlayButton, width/2, height/1.2, howToPlayButtonWidth, howToPlayButtonHeight);
+  image(howToPlayButton, xHowToPlayButton, yHowToPlayButton, howToPlayButtonWidth, howToPlayButtonHeight);
 }
 function displayStartTitle() {
   image(title, width / 2, height / 4, 400, 200);
 }
-function displayHoverPlayButton() {
-  image(howToPlayButton, width/2, height/1.7, playButtonWidth, playButtonHeight);
+function displayHoverPlay() {
+  image(hoverPlay, xPlayButton, yPlayButton, playButtonWidth, playButtonHeight);
+}
+function displayHoverHowToPlay() {
+  image(hoverHowToPlay, xHowToPlayButton, yHowToPlayButton, howToPlayButtonWidth, howToPlayButtonHeight);
 }
 
 ///////////// Game = Play Functions /////////////////////
+
+function hoverStartButtons() {
+  if (mouseX > xPlayButton - playButtonWidth/2 && mouseX < xPlayButton + playButtonWidth/2 && mouseY > yPlayButton - playButtonHeight/2 && mouseY < yPlayButton + playButtonHeight/2) {
+    displayHoverPlay();
+  }
+  else {
+    displayPlayButton();
+  }
+  if (mouseX > xHowToPlayButton - howToPlayButtonWidth/2 && mouseX < xHowToPlayButton + howToPlayButtonWidth/2 && mouseY > yHowToPlayButton - howToPlayButtonHeight/2 && mouseY < yHowToPlayButton + howToPlayButtonHeight/2) {
+    displayHoverHowToPlay();
+  }
+  else {
+    displayHowToPlayButton();
+  }
+}
 
 function hoverCards() {
   if (
@@ -185,16 +215,11 @@ function hoverCards() {
   }
 }
 
-function hoverStartButtons() {
-  if (
-    mouseX > width/2 - playButtonWidth / 2 &&
-    mouseX < width/2 + playButtonWidth / 2 &&
-    mouseY > yCardS - playButtonHeight / 2 &&
-    mouseY < yCardS + playButtonHeight / 2
-  ) {
-    playButtonHover = true;
-  } 
-  else {
-    playButtonHover = false;
+////////////// Press Buttons ///////////////
+
+function pressPlay() {
+  if (mouseX > xPlayButton - playButtonWidth/2 && mouseX < xPlayButton + playButtonWidth/2 && mouseY > yPlayButton - playButtonHeight/2 && mouseY < yPlayButton + playButtonHeight/2 && mouseIsPressed) {
+    state = "play";
   }
 }
+
