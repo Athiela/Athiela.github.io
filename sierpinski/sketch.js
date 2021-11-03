35ae2fcf0773d628bc244bc1fa7e34efe7113312
Athiela A <athiela_arcamo@yahoo.ca>
2021-11-02 23:54:54 -0600

    sierpinsky triangle demo

--- sierpinski/sketch.js
+++ sierpinski/sketch.js
@@ -1,13 +1,13 @@
-// Sierpinski triangle
+// Sierpinski Triangle Demo
 // Recursion visualization
 
 let triangleVertices = [
-  {x:400, y:100},
-  {x:100, y:700},
-  {x:700, y:700},
+  {x: 400, y: 100},
+  {x: 100, y: 700}, 
+  {x: 700, y: 700},
 ];
 
-let depth = 1;
+let depth = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -23,20 +23,32 @@ function mousePressed() {
 }
 
 function sierpinski(points, degree) {
-  let theColours = ["blue", "red", "yellow", "purple", "green", "black", "orange"];
-  fill(theColours[degree]);
+  let theColors = ["blue", "red", "green", "white", "yellow", "violet", "orange", "black"];
+  fill(theColors[degree]);
+  noStroke();
   triangle(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y);
 
   if (degree > 0) {
-    sierpinski([points[0], getMidPoint(points[0], points[1]), getMidPoint(points[0], points[2]), degree-1]);
-    sierpinski([points[1], getMidPoint(points[0], points[1]), getMidPoint(points[1], points[2]), degree-1]);
-    sierpinski([points[2], getMidPoint(points[0], points[2]), getMidPoint(points[1], points[2]), degree-1]);
+    sierpinski([points[0],
+                getMidpoint(points[0], points[1]),
+                getMidpoint(points[0], points[2])],
+                degree-1);
+
+    sierpinski([points[1],
+                getMidpoint(points[0], points[1]),
+                getMidpoint(points[1], points[2])],
+                degree-1);
+
+    sierpinski([points[2],
+                getMidpoint(points[0], points[2]),
+                getMidpoint(points[1], points[2])],
+                degree-1);
   }
 }
 
-function getMidPoint(point1, point2) {
-  let xDiff = (point1.x+point2.x)/2;
-  let yDiff = (point1.y+point2.y)/2;
-  let midPoint = {x: xDiff, y:yDiff};
-  return midPoint;
+function getMidpoint(point1, point2) {
+  let xDiff = (point1.x + point2.x)/2;
+  let yDiff = (point1.y + point2.y)/2;
+  let midpoint = {x: xDiff, y: yDiff};
+  return midpoint;
 }
